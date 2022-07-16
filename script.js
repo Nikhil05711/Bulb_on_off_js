f7a249f0323a78d0d45cd32103416124ab953194
Nikhil05711 <Nikhil@123>
2022-07-16 15:47:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+function bulbToggle() {
+  let bulb = document.getElementById("lightbulb");
+
+  if (bulb.src.match("lightoff")) {
+    bulb.src = "lighton.jpg";
+  } else {
+    bulb.src = "lightoff.jpg";
+  }
+}
+function bulbToggle1() {
+  let bulb = document.getElementById("lightbulb1");
+
+  if (bulb.src.match("lightoff")) {
+    bulb.src = "lighton.jpg";
+  } else {
+    bulb.src = "lightoff.jpg";
+  }
+}
+function bulbToggle2() {
+  let bulb = document.getElementById("lightbulb2");
+
+  if (bulb.src.match("lightoff")) {
+    bulb.src = "lighton.jpg";
+  } else {
+    bulb.src = "lightoff.jpg";
+  }
+}
+function bulbToggle3() {
+  let bulb = document.getElementById("lightbulb3");
+
+  if (bulb.src.match("lightoff")) {
+    bulb.src = "lighton.jpg";
+  } else {
+    bulb.src = "lightoff.jpg";
+  }
+}
+function bulbToggle4() {
+  let bulb = document.getElementById("lightbulb4");
+
+  if (bulb.src.match("lightoff")) {
+    bulb.src = "lighton.jpg";
+  } else {
+    bulb.src = "lightoff.jpg";
+  }
+}
+function bulbToggle5() {
+  let bulb = document.getElementById("lightbulb5");
+
+  if (bulb.src.match("lightoff")) {
+    bulb.src = "lighton.jpg";
+  } else {
+    bulb.src = "lightoff.jpg";
+  }
+}
